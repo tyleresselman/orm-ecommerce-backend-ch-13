fe394adc08b5f965863c4da795aec5e3dac06f9b
Tyler Esselman <tme91302@gmail.com>
2022-10-07 19:59:09 -0700

    finished remaining routes, building insomnia tests now

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Category.findAll({
-      include: [{ model: Product}, { model: Tag, through: ProductTag }]
+    const productData = await Product.findAll({
+      include: [{ model: Category}, { model: Tag, through: ProductTag }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -22,8 +22,8 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product}, { model: Tag, through: ProductTag }]
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, { model: Tag, through: ProductTag }]
     });
 
     if(!productData) {
@@ -40,10 +40,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
