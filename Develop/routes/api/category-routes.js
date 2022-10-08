07c664c6189765d71b51f85819a6ffd894af2d8d
Tyler Esselman <tme91302@gmail.com>
2022-10-07 18:15:00 -0700

    finished category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -45,12 +45,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.put('/:id', async (req, res) => {
-//   // update a category by its `id` value
-//   try {
-
-//   }
-// });
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({
+      category_name: req.body.category_name,
+    },
+      {
+        where: {
+          id: req.params.id
+        },
+      }
+    );
+    if(!categoryData) {
+      res.status(404).json({ message: `Sorry, no categories by that name 'round these parts...`});
+      return;
+    }
+    req.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
